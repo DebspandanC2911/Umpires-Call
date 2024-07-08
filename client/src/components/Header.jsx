1f8b8578ab650083c48c80ac03b1e9a6b3b20b58
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-07-08 19:01:23 +0530

    Adding index file and nodemon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
                 />
             </Button>
             <Link to="/sign-in">
-                <Button gradientDuoTone="purpleToBlue">
+                <Button gradientDuoTone="purpleToBlue" outline>
                     Sign-In
                 </Button>
             </Link>
