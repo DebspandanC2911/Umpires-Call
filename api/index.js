9424f34f8a150b24d636a96675233b899666c3e8
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-07-08 23:27:10 +0530

    Created signup APIs

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import UserRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(
     ()=>{
@@ -12,8 +12,12 @@ mongoose.connect(process.env.MONGO).then(
         console.log(err);
     });
 const app=express();
+
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log("server is running on port 3000 !!!");
 });
 
 app.use('/api/user',UserRoutes);
+app.use('/api/auth',authRoutes);
