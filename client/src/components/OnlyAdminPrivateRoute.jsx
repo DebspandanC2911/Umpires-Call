63834c27ad35bfa06a1bc0fac8664e3c387a4725
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-07-10 04:56:25 +0530

    Added "Create Post" UI

--- client/src/components/OnlyAdminPrivateRoute.jsx
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -3,5 +3,9 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 export default function OnlyAdminPrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser.isAdmin ? <Outlet /> : <Navigate to='/sign-in' />;
+  return currentUser && currentUser.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/sign-in' />
+  );
 }
