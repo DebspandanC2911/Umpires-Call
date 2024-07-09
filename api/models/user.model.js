096bbc13d2ffed0576257b1b8d9a8335fa5d218d
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-07-10 04:13:27 +0530

    Add Admin Functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,28 +1,34 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        min:5,
-        max:20,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    profilePicture:{
-        type:String,
-        default: "https://www.thesportsmirror.com/wp-content/uploads/2023/02/Best-Cricket-Team.jpg",
-    }
-}, {timestamps:true});
+    profilePicture: {
+      type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
