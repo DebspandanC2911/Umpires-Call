660bf2876c2f9d2ee65fe259f806b143ee5b6b94
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-07-10 05:23:00 +0530

    Add a "Create Post" API Route

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://theblogreview.com/wp-content/uploads/2019/08/The-Blog-Review-Top-Cricket-Blogs.jpeg',
+    },
+    category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
