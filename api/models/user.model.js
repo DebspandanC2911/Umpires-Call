65fb0482519096c49fdd3f08f9a977ec9a417708
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-07-16 21:29:50 +0530

    Fixing the Recent Articles section

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     profilePicture: {
       type: String,
       default:
-        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.rediff.com%2Fcricket%2Freport%2Fplayers-barred-from-using-india-logo-in-domestic-cricket%2F20141207.htm&psig=AOvVaw34l394i0TyIiZqgD9mzSri&ust=1721228888726000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIi_l7rrq4cDFQAAAAAdAAAAABAE',
     },
     isAdmin: {
       type: Boolean,
