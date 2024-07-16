dc91221f7b5b26013248c88ca0ab65962654c885
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-07-16 20:00:55 +0530

    Fixing Posts on HomePage

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,12 +8,14 @@ import {
   updateUser,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+import { getposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.put('/getUser',verifyToken,getUser);
+router.put('/getposts',verifyToken,getposts);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
